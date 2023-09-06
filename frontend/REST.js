6bcc01e0f51f61771d72e0a05fa602130454f10d
Marcus Jaes <marcus.jaes@gmail.com>
2023-09-06 15:06:37 +0200

    start på frontend, objekter 100% korrekt nu

--- frontend/REST.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const artistDatabase = "http://localhost;3333";
-
-async function getArtists(params) {
-    const artistsFromDatabase = await fetch(`${artistDatabase}/artists`)
-}
--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,14 @@
 "use strict";
 
+const artistDatabase = "http://localhost;3333";
+
 window.addEventListener("load", initApp);
 
 function initApp(params) {
     
+}
+
+
+async function getArtists(params) {
+  const artistsFromDatabase = await fetch(`${artistDatabase}/artists`);
 }
